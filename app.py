e643d1f044240c9ea35b20fa3df70d5ef077dc24
MReDDoT <mohammad.mehdi.monsef@protonmail.com>
2024-10-24 23:24:38 +0330

    test

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///questions.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
-
+#hello
 with app.app_context():
     db.create_all()
 
