3ffdcc056bee9905892aa3f9605b1ada0c413945
MReDDoT <mohammad.mehdi.monsef@protonmail.com>
2024-10-25 18:30:00 +0330

    debug all the code exept login,register and fixed admin question system

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, session
 from models import db, Question
+import sqlite3
+import re
 
+
+'''Fatemeh code:'''
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///questions.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -8,11 +12,22 @@ db.init_app(app)
 
 with app.app_context():
     db.create_all()
+#Questions show
+@app.route('/question',methods=['GET', 'POST'])
+def category_index():
+    if request.method == 'POST':
+        category=request.form['category']
+        return redirect(f'/question/{category}')
+    return render_template('category_index.html')
 
-@app.route('/question')
-def index():
-    questions = Question.query.all()
-    return render_template('index.html', questions=questions)
+@app.route('/question/<category>')
+def index(category):
+    category=category
+    questions = Question.query.filter(Question.category==category)
+    if category!='math' and category!='historical' and category!='game' and category!='soccer' and category!='movie' and category!='english' and category!='general_information':
+        return render_template('404.html')
+    else:
+        return render_template('index.html', questions=questions)
 
 @app.route('/question/<int:question_id>', methods=['GET', 'POST'])
 def question(question_id):
@@ -32,13 +47,14 @@ def add_question():
         option3 = request.form['option3']
         option4 = request.form['option4']
         correct_answer = request.form['correct_answer']
+        category=request.form['category']
 
         new_question = Question(text=text, option1=option1, option2=option2,
                                 option3=option3, option4=option4,
-                                correct_answer=correct_answer)
+                                correct_answer=correct_answer,category=category)
         db.session.add(new_question)
         db.session.commit()
-        return redirect('/')
+        return redirect('/question')
     return render_template('add_question.html')
 
 @app.route('/delete_question/<int:question_id>', methods=['POST'])
@@ -46,8 +62,45 @@ def delete_question(question_id):
     question = Question.query.get_or_404(question_id)
     db.session.delete(question)
     db.session.commit()
-    return redirect('/')
+    return redirect('/question')
 
+@app.route('/edit_question/<int:question_id>', methods=['GET', 'POST'])
+def edit_question(question_id):
+    conn = sqlite3.connect("instance/questions.db")
+    cursor = conn.execute('SELECT * FROM question WHERE id = ?',(question_id,))
+    
+    for row in cursor:
+        edit_question=row[1]
+        edit_awnser1=row[2]
+        edit_awnser2=row[3]
+        edit_awnser3=row[4]
+        edit_awnser4=row[5]
+        edit_correct_awnser=row[6]
+        edit_category=row[7]
+    try:
+        if request.form ['submit_button']=='اعمال تغییرات سوال':
+            text = request.form['text']
+            option1 = request.form['option1']
+            option2 = request.form['option2']
+            option3 = request.form['option3']
+            option4 = request.form['option4']
+            correct_answer = request.form['correct_answer']
+            category = request.form['category']
+            conn.execute('''UPDATE question SET text = ?, option1 = ?, option2 = ?, option3 = ?, option4 = ?, correct_answer = ?,  category = ? WHERE id = ?''',(text, option1,option2,option3,option4,correct_answer,category,question_id))
+            conn.commit()
+            conn.close()
+            return redirect('/question')
+    except:
+        return render_template('edit_question.html', edit_question=edit_question,
+                           edit_awnser1=edit_awnser1,
+                           edit_awnser2=edit_awnser2,
+                           edit_awnser3=edit_awnser3,
+                           edit_awnser4=edit_awnser4,
+                           edit_correct_awnser=edit_correct_awnser,
+                           category=edit_category)
+    
+
+'''Nima code:'''
 # Database building
 def init_sqlite_db():
     conn = sqlite3.connect("users.db")
@@ -56,7 +109,8 @@ def init_sqlite_db():
         """CREATE TABLE IF NOT EXISTS users (
                         id INTEGER PRIMARY KEY AUTOINCREMENT,
                         username TEXT NOT NULL,
-                        password TEXT NOT NULL)"""
+                        password TEXT NOT NULL,
+                        email TEXT)"""
     )
     conn.commit()
     conn.close()
@@ -69,7 +123,6 @@ init_sqlite_db()
 # Registration
 
 
-#@app.route("/")
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
@@ -96,8 +149,8 @@ def register():
 
         try:
             cursor.execute(
-                "INSERT INTO users (username, password) VALUES (?, ?)",
-                (username, password),
+                "INSERT INTO users (username, password, email) VALUES (?, ?, ?)",
+                (username, password, 'None'),
             )
             conn.commit()
             flash("Registration was successful.Now You can login.")
--- models.py
+++ models.py
@@ -10,6 +10,7 @@ class Question(db.Model):
     option3 = db.Column(db.String(100), nullable=False)
     option4 = db.Column(db.String(100), nullable=False)
     correct_answer = db.Column(db.String(100), nullable=False)
+    category = db.Column(db.String(100), nullable=False)
 
     def __repr__(self):
         return f'<Question {self.text}>'
